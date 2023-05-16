83329731add859d7b51e630001dce5dc3e471756
João Pedro Bove da Costa <joao.bove@hotmail.com>
2023-05-16 16:57:46 -0300

    Counting repeated numbers
    added a method that counts each time the number is repeated.

--- DoubleIndex.java
+++ DoubleIndex.java
@@ -1,6 +1,8 @@
 package sorting;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 public class DoubleIndex {
 
@@ -8,8 +10,6 @@ public class DoubleIndex {
 
 		return segregate(nums, num, nums.size(), 0);
 	}
-
-	
 	// método depois da primeira tentativa
 	public static int segregate(ArrayList<Integer> nums, int num, int fim, int inicio) {
 
@@ -20,28 +20,75 @@ public class DoubleIndex {
 
 		int end = fim, start = inicio;
 		int position = (start + end)/2;
-		
+
 		//Condição de não achar o numero
 		if(start > end) {
 			return -1;
 		}
-		
+
 		if(array.get(position) == number) {
+
 			// se for o numero certo
+			repeatedNumber(nums, position, number);
+
+			System.out.println("The number's position is: ");
+
 			return position;
 
 		}
-
 		// Procura na metado menor
 		if( array.get(position) > number) {
 
 			return segregate(nums, number, position - 1, start);
+
 		} 
+
 		// se o numero da posicao atual for menor que o numero
 		else {
 
 			return segregate(nums, number, end, position + 1);
+
 		}
+
 		// recursividade
+	}
+	public static void repeatedNumber(ArrayList<Integer> nums,int position, int number) {
+		ListIterator<Integer> it = nums.listIterator(position);
+
+		int equalNumbers = 0;
+
+		//If there's repeated numberes this method counts and print them
+		try {
+
+			while (it.previous() == number) {
+				equalNumbers++;
+			}
+
+		}
+		catch(NoSuchElementException Exception) {
+
+		}
+
+		try {
+
+			it = nums.listIterator(position);
+			while(it.next() == number) {
+				equalNumbers++;
+
+			}
+
+		}
+		catch(NoSuchElementException Exception) {
+
+		}
+
+		if(equalNumbers == 1) {
+			System.out.println("There's one number " + number + " in the list\n");
+			return;
+		} 
+		else {
+			System.out.println("There are " + equalNumbers + " number " + number + "\n");
+			return;
+		}
 	}
 }
